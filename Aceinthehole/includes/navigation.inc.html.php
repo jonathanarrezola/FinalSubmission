cb708beebc669a25e9856a392fae3e824e998d60
jonathanarrezola <78997143+jonathanarrezola@users.noreply.github.com>
2021-03-16 23:14:24 -0700

    Add files via upload

---
+++ Aceinthehole/includes/navigation.inc.html.php
@@ -0,0 +1,29 @@
+<nav class="topnav" id="myTopnav">
+  <a href="../home/home.html.php" style="color:red;
+	text-decoration: none;
+	background-color: white;
+	border-radius: 10%;
+	padding: 5px;
+" class="active">Home</a>
+  <a href="../events/events.inc.html.php" style="color:red;
+	text-decoration: none;
+	background-color: white;
+	border-radius: 10%;
+	padding: 5px;
+" class="active">Events</a>
+  <a href="../register/register.inc.html.php" style="color:red;
+	text-decoration: none;
+	background-color: white;
+	border-radius: 10%;
+	padding: 5px;
+" class="active">Register for Events</a>
+  <a href="../FAQ/FAQ.inc.html.php" style="color:red;
+	text-decoration: none;
+	background-color: white;
+	border-radius: 10%;
+	padding: 5px;
+" class="active">Questions/FAQ's</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</nav>
